c63e045944141d51c9eb652c55fb3b2b11167e54
OstapS <shvalev_o_s@ntik.ru>
2025-07-08 22:41:02 +0300

    fix _promt

--- development/projects/GPT-Agents/chapter_2/lmstudio_server.py
+++ development/projects/GPT-Agents/chapter_2/lmstudio_server.py
@@ -4,12 +4,10 @@ from openai import OpenAI
 client = OpenAI(base_url="http://172.21.144.1:1234/v1", api_key="not-needed")
 
 completion = client.chat.completions.create(
-  model="TheBloke/Llama-2-7B-Chat-GGUF", # this field is currently unused
+  model="llama-2-7b-chat", # this field is currently unused
   messages=[
     {"role": "system", "content": "Always answer in russian."},
-    {"role": "user", "content": "Introduce yourself."},
-    {"role": "assistant", "content": "Hello, I am a helpful assistant."},
-    {"role": "user", "content": "What is the capital of France?"}
+    {"role": "user", "content": "Столица России?"}
   ],
   temperature=0.7,
 )
